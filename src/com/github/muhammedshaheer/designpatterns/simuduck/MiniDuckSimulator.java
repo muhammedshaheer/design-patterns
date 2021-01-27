c48329eb9646a9b5d042e6d94836b7f133dd7d9d
Muhammed Shaheer <ckshaheer2012@gmail.com>
2021-01-27 11:41:54 +0530

    dynamic behaviour implemented

--- src/com/github/muhammedshaheer/designpatterns/simuduck/MiniDuckSimulator.java
+++ src/com/github/muhammedshaheer/designpatterns/simuduck/MiniDuckSimulator.java
@@ -1,10 +1,17 @@
 package com.github.muhammedshaheer.designpatterns.simuduck;
 
+import com.github.muhammedshaheer.designpatterns.simuduck.fly.FlyRocketPowered;
+
 public class MiniDuckSimulator {
 
     public static void main(String[] args) {
         Duck mallardDuck = new MallardDuck();
         mallardDuck.performQuack();
         mallardDuck.performFly();
+
+        Duck modelDuck = new ModelDuck();
+        modelDuck.performFly();
+        modelDuck.setFlyBehaviour(new FlyRocketPowered());
+        modelDuck.performFly();
     }
 }
